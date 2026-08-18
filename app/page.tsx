a78aa99e5d99a2b87cf9e842b58ca2a82a40a1c4
Zsuzsa.L <manoaway@gmail.com>
2026-08-18 20:42:03 +0200

    feat: change font, add title and footer

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
 <>
-      <header className="">
+      <header className="p-8">
         <Image
           className=""
           src="/as.svg"
-          alt="about source logo"
+          alt="aboutsource logo"
           width={100}
           height={20}
           priority
         />
-
       </header>
+<main className="p-8">
+<h1>GitHub Dashboard</h1>
+</main>
+<footer>Made by: Zsuzsa Lukacs {new Date().getFullYear()}</footer>
 </>
   
   );
