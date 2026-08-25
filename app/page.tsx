0f4d092cb7050b99c91d97174a714de560647141
Zsuzsa.L <manoaway@gmail.com>
2026-08-25 11:10:46 +0200

    perf(repos): use Suspense + use() instead of useEffect pattern

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-"use client";
 import Reposatories from "@/components/Reposatories";
+import { getRepositories } from "@/lib/github";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Suspense } from "react";
 
 export default function Home() {
-  const [repos, setRepos] = useState([]);
-
-  useEffect(() => {
-    fetch("/api/repos")
-      .then((response) => response.json())
-      .then((data) => setRepos(data));
-  }, []);
+  const reposPromise = getRepositories();
 
   return (
     <>
@@ -19,7 +13,19 @@ export default function Home() {
       </header>
       <main>
         <h1>GitHub Dashboard</h1>
-        <Reposatories repos={repos} />
+        <section className="max-w-2xl m-auto">
+          <h2> Reposatories </h2>
+          <Suspense
+            fallback={
+              <>
+                <p className="animate-spin inline-block my-8 mx-2">↻</p>{" "}
+                <span>Loading list of repos</span>
+              </>
+            }
+          >
+            <Reposatories reposPromise={reposPromise} />
+          </Suspense>
+        </section>
       </main>
       <footer>Made by: Zsuzsa Lukacs {new Date().getFullYear()}</footer>
     </>
--- components/Reposatories.tsx
+++ components/Reposatories.tsx
@@ -1,20 +1,20 @@
+"use client";
 import { Repository } from "@/types/repo";
 import Repo from "./Repo";
+import { use } from "react";
 
 type RepositoriesProps = {
-  repos: Repository[];
+  reposPromise: Promise<Repository[]>;
 };
 
-const Reposatories = ({ repos }: RepositoriesProps) => {
+const Reposatories = ({ reposPromise }: RepositoriesProps) => {
+  const repos = use(reposPromise);
   return (
-    <section className="max-w-2xl m-auto">
-      <h2> Reposatories </h2>
-      <ul className="accordion my-2">
-        {repos.map((repo) => (
-          <Repo repo={repo} key={repo.id} />
-        ))}
-      </ul>
-    </section>
+    <ul className="accordion my-2">
+      {repos.map((repo) => (
+        <Repo repo={repo} key={repo.id} />
+      ))}
+    </ul>
   );
 };
 
